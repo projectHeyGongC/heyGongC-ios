a2e416bbe29d635cecc0c67e4b6dfd8e991b1f85
EunSeo <alice9547@gmail.com>
2023-12-25 15:55:56 +0900

    first commit

---
+++ heyGongC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  heyGongC
+//
+//  Created by 김은서 on 12/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
