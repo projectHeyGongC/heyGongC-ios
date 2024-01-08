f2cf1a93e2c920cb165ad107e7d5bd5b5a49510c
alice9547@naver.com <kes@quantec.co.kr>
2024-01-08 15:03:38 +0900

    모니터링 화면 레이아웃 작업

--- heyGongC/Classes/Localized.swift
+++ heyGongC/Classes/Localized.swift
@@ -11,6 +11,10 @@ import SwiftyUserDefaults
 // MARK: - Localized
 enum Localized {
     case ERROR_MSG
+    case MONITORING_CAMERA_OFF
+    case MONITORING_CAMERA_ON
+    case MONITORING_NOISE_ON
+    case MONITORING_NOISE_OFF
 
     /**
      *  다국어 처리
@@ -19,6 +23,14 @@ enum Localized {
         switch self {
         case .ERROR_MSG:
             return "잠시 후 시도해주세요"
+        case .MONITORING_CAMERA_OFF:
+            return "카메라가 꺼져 있습니다."
+        case .MONITORING_CAMERA_ON:
+            return "집 이미지를 클릭하면 모니터링이 시작됩니다."
+        case .MONITORING_NOISE_ON:
+            return "소리 감지 모드 on"
+        case .MONITORING_NOISE_OFF:
+            return "소리 감지 모드 off"
         }
     }
 }
