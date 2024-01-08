be508fbd05f5373bfd39d514b2cfa654acedb4ef
alice9547@naver.com <kes@quantec.co.kr>
2024-01-08 10:52:41 +0900

    gitIgnore 재적용 중

---
+++ heyGongC/Classes/DefaultsKeys.swift
@@ -0,0 +1,20 @@
+//
+//  DefaultsKeys.swift
+//  heyGongC
+//
+//  Created by 김은서 2023/12/25.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+/**
+ *  SharedPreferenceUtil.java
+ */
+                                                    
+extension DefaultsKeys {
+    //기본정보
+//    var AUTH_TOKEN: DefaultsKey<String> { .init("AUTH_TOKEN", defaultValue:"") }
+    
+    var IS_PUSH: DefaultsKey<Bool> { .init("IS_PUSH", defaultValue: false) }            // kes 230323 푸시 왔는지 체크 후 상단바 이미지 on/off
+}
---
+++ heyGongC/Classes/Segue.swift
@@ -0,0 +1,77 @@
+//
+//  Segue.swift
+//  heyGongC
+//
+//  Created by 김은서 2023/12/25.
+//
+import UIKit
+import SafariServices
+import SwiftyUserDefaults
+
+struct Storyboard {
+    static let Splash                = "Splash"
+    static let Login                 = "Login"
+    static let Monitoring            = "Monitoring"
+    static let Analysis              = "Analysis"
+    static let Subscription          = "Subscription"
+    static let UserProfile           = "UserProfile"
+}
+
+class SegueUtils {
+    /// 일반적으로 push 할 때 사용_ 전달되는 파라미터 없음
+    /// - Parameters:
+    ///   - target: 현재 화면
+    ///   - link: 이동할 화면 (Link)
+    ///   - animated: 애니메이션 유무
+    static func open(target: UIViewController, link: Link, animated: Bool = true) {
+        if let vc = link.viewController {
+            target.navigationController?.pushViewController(vc, animated: animated)
+        }
+    }
+}
+
+extension SegueUtils {
+    
+    // 상품 상세 이동
+//    static func goToProductDetail(target: BaseVC, productInfoList: [PortBasicInfo], investType: INVEST_TYPE) {
+//        if let vc = Link.ProductDetailVC.viewController as? ProductDetailVC {
+//            vc.updateParam(productInfoList: productInfoList, invType: investType)
+//            target.navigationController?.pushViewController(vc, animated: true)
+//        }
+//    }
+    
+}
+
+enum Link: String {
+    case CreateAccountVC        = "CreateAccountVC"
+    
+    var baseStoryboard: String {
+        switch self {
+        case .CreateAccountVC:
+            return Storyboard.Login
+        }
+    }
+    
+    var url: String {
+        return self.rawValue
+    }
+    
+    var viewController: UIViewController? {
+        guard let sb = self.storyboard else { return nil }
+        guard let id = self.segue else { return nil }
+        
+        return sb.instantiateViewController(identifier: id)
+    }
+    
+    var baseViewController: BaseVC? {
+        return viewController as? BaseVC
+    }
+    
+    var storyboard: UIStoryboard? {
+        return UIStoryboard(name: self.baseStoryboard, bundle: nil)
+    }
+    
+    var segue: String? {
+        return self.rawValue
+    }
+}
