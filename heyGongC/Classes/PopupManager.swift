a93726e0523923e64ff309ecddd84c624f04f949
Yeji-Jang1210 <jyg8727@gmail.com>
2024-01-15 16:18:50 +0900

    팝업 매니저 코드 수정 +악센트 컬러 추가 +컨트롤러 코드 수정

--- heyGongC/Classes/PopupManager.swift
+++ heyGongC/Classes/PopupManager.swift
@@ -36,23 +36,39 @@ extension UIViewController {
     }
     
     func showAlert(localized: Localized,
+                   isAccent: Bool = false,
                    confirm: (()->())? = nil,
                    cancel: (()->())? = nil) {
         
         let alert = UIAlertController(title: localized.title, message: localized.txt, preferredStyle: .alert)
         
-        if localized.confirmText == "" {
+        let textFont = UIFont.systemFont(ofSize: 17, weight: .regular)
+        let attributes = NSMutableAttributedString(string: localized.title)
+        
+        if isAccent {
+            let accentTitle = localized.title.split(separator: "\n")
+            if accentTitle.count > 1 {
+                attributes.addAttribute(.foregroundColor, value: UIColor.red, range: (localized.title as NSString).range(of: "\(accentTitle[1])"))
+            }
+            
+            attributes.addAttribute(.font, value: textFont, range: (localized.title as NSString).range(of: "\(localized.title)"))
+            alert.setValue(attributes, forKey: "attributedTitle")
+        }
+        
+        
+        if localized.confirmText != "" {
             let confirmAction = UIAlertAction(title: localized.confirmText, style: .default){ action in
                 confirm?()
             }
+            confirmAction.setValue(UIColor(red: 0/255, green: 104/255, blue: 119/255, alpha: 1), forKey: "titleTextColor")
             alert.addAction(confirmAction)
         }
         
-        if localized.cancelText == "" {
+        if localized.cancelText != "" {
             let cancelAction = UIAlertAction(title: localized.cancelText, style: .cancel){ action in
                 cancel?()
             }
-            
+            cancelAction.setValue(UIColor(red: 0, green: 0, blue: 0, alpha: 0.5), forKey: "titleTextColor")
             alert.addAction(cancelAction)
         }
         
