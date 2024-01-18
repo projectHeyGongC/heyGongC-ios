2344e1eaf410d5c74b846c4d78b35bb05c562dd5
Yeji-Jang1210 <jyg8727@gmail.com>
2024-01-18 16:52:24 +0900

    UI작업 및 QR스캔 구현

---
+++ heyGongC/Classes/DeviceNamingVM.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceNamingVM.swift
+//  heyGongC
+//
+//  Created by 장예지 on 1/17/24.
+//
+
+import Foundation
+import Foundation
+import RxSwift
+import RxCocoa
+
+class DeviceNamingVM: BaseVM {
+    
+}
---
+++ heyGongC/Classes/QRCodeReaderVM.swift
@@ -0,0 +1,15 @@
+//
+//  QRCodeReaderVM.swift
+//  heyGongC
+//
+//  Created by 장예지 on 1/17/24.
+//
+
+import Foundation
+import Foundation
+import RxSwift
+import RxCocoa
+
+class QRCodeReaderVM: BaseVM {
+    
+}
