07b20934ccf5da284f6c327d58b24f72644aa5b6
김은서 <alice9547@naver.com>
2024-02-07 17:02:37 +0900

    애플로그인 작업중

--- heyGongC/Classes/SelectAccountTypeVC.swift
+++ heyGongC/Classes/SelectAccountTypeVC.swift
@@ -11,6 +11,7 @@ import SnapKit
 import RxSwift
 import SwiftyUserDefaults
 import GoogleSignIn
+import AuthenticationServices
 
 
 class SelectAccountTypeVC: BaseVC {
@@ -79,6 +80,17 @@ extension SelectAccountTypeVC {
             // 서버에 토큰을 보내기. 이 때 idToken, accessToken 차이에 주의할 것
         }
     }
+    
+    private func loginForApple() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email] //유저로 부터 알 수 있는 정보들(name, email)
+               
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
 }
 
 // MARK: - Delegate
@@ -104,5 +116,59 @@ extension SelectAccountTypeVC: UICollectionViewDelegateFlowLayout {
         let width = scrollView.frame.width
         currentPage = Int(scrollView.contentOffset.x / width)
         pageController.currentPage = currentPage
+    }
+}
+
+// MARK: - AppleLoginDelegate
+extension SelectAccountTypeVC: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding{
+  func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+    //로그인 성공
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            // You can create an account in your system.
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            if  let authorizationCode = appleIDCredential.authorizationCode,
+                let identityToken = appleIDCredential.identityToken,
+                let authCodeString = String(data: authorizationCode, encoding: .utf8),
+                let identifyTokenString = String(data: identityToken, encoding: .utf8) {
+                print("authorizationCode: \(authorizationCode)")
+                print("identityToken: \(identityToken)")
+                print("authCodeString: \(authCodeString)")
+                print("identifyTokenString: \(identifyTokenString)")
+            }
+            
+            print("useridentifier: \(userIdentifier)")
+            print("fullName: \(fullName)")
+            print("email: \(email)")
+            
+            //Move to MainPage
+            //let validVC = SignValidViewController()
+            //validVC.modalPresentationStyle = .fullScreen
+            //present(validVC, animated: true, completion: nil)
+            
+        case let passwordCredential as ASPasswordCredential:
+            // Sign in using an existing iCloud Keychain credential.
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            print("username: \(username)")
+            print("password: \(password)")
+            
+        default:
+            break
+        }
+    }
+    
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // 로그인 실패(유저의 취소도 포함)
+        print("login failed - \(error.localizedDescription)")
     }
 }
