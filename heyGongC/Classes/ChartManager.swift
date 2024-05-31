86e821470944cbb1275c770e79347f6160009f86
김은서 <alice9547@naver.com>
2024-05-31 17:01:29 +0900

    차트 작업중

--- heyGongC/Classes/ChartManager.swift
+++ heyGongC/Classes/ChartManager.swift
@@ -17,23 +17,46 @@ class ChartManager {
     ///   - entryArray: 차트에 넣을 데이터
     ///   - barColor: bar 색
     public func drawChart(barChartView: BarChartView, entryArray: [BarChartDataEntry], barColor: UIColor) {
+        
+        // 스크롤 및 확대/축소 설정
+        barChartView.dragEnabled = true
+        barChartView.setScaleEnabled(false)
+        barChartView.scaleYEnabled = false // y축 방향 확대/축소 비활성화
+        barChartView.scaleXEnabled = true  // x축 방향 확대/축소 활성화
+        
+        // 가로 스크롤만 가능하도록 설정
+        barChartView.pinchZoomEnabled = false
+        barChartView.highlightPerDragEnabled = true
+        
+        // x축 스크롤 범위 설정 (6시간)
+        let visibleRange = 72 // 6시간에 해당하는 데이터 수 (5분 간격으로 총 72개의 데이터)
+        let totalRange = 24 * 60 / 5 // 전체 시간 범위 (5분 간격으로 총 288개의 데이터)
+        let initialVisibleStart = totalRange - visibleRange // 초기에 표시할 범위 시작 지점 (00:00시로 설정)
+
+        barChartView.setVisibleXRangeMaximum(Double(visibleRange))
+        barChartView.moveViewToX(Double(0))
+        
+        // No data 설정
         barChartView.noDataText = "데이터가 없습니다."
         barChartView.noDataFont = .systemFont(ofSize: 20, weight: .bold)
-        barChartView.dragXEnabled = true
-        barChartView.dragYEnabled = true
         barChartView.doubleTapToZoomEnabled = false
         barChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
         
-        barChartView.xAxis.setLabelCount(entryArray.count, force: false)
+        // 범례 숨기기
+        barChartView.legend.enabled = false
+        
+        barChartView.xAxis.setLabelCount(entryArray.count/12, force: false)
         barChartView.xAxis.drawAxisLineEnabled = false
         barChartView.xAxis.drawGridLinesEnabled = false
         barChartView.xAxis.drawLabelsEnabled = true
-        barChartView.xAxis.axisLineWidth = 3
-
+        barChartView.xAxis.granularity = 12
+        
         barChartView.leftAxis.drawAxisLineEnabled = false
         barChartView.leftAxis.drawGridLinesEnabled = true
         barChartView.leftAxis.drawLabelsEnabled = true
-
+        barChartView.leftAxis.granularity = 1.0
+        barChartView.leftAxis.axisMinimum = 0 // 최소 y값 설정
+        
         barChartView.rightAxis.drawAxisLineEnabled = false
         barChartView.rightAxis.drawGridLinesEnabled = true
         barChartView.rightAxis.drawLabelsEnabled = false
@@ -41,8 +64,10 @@ class ChartManager {
         let chartDataSet = BarChartDataSet(entries: entryArray, label: "알림 횟수")
         chartDataSet.colors = [barColor]
         chartDataSet.highlightEnabled = false
+        chartDataSet.drawValuesEnabled = false // 바에 값 표시하지 않음
         
         let chartData = BarChartData(dataSet: chartDataSet)
+        chartData.barWidth = 2.5
         barChartView.data = chartData
     }
     
